8ff554bff4abcf9539741c86f2ca3b3f29a901d6
Alejo Lopez <manallop@bancolombia.com.co>
2023-04-09 16:21:13 -0500

    solution for exercise number 7

--- app/src/main/java/co/com/alejoloopez5/challenges/increasingsequence.kt
+++ app/src/main/java/co/com/alejoloopez5/challenges/increasingsequence.kt
@@ -1,30 +1,36 @@
 package co.com.alejoloopez5.challenges
 
+import java.lang.Math.abs
+
 fun main(){
-    solution(mutableListOf(1, 2, 1, 2))
-    solution(mutableListOf(3, 6, 5, 8, 10, 20, 15))
+
+    solution(mutableListOf(1, 2, 1, 2)) //false
+    solution(mutableListOf(1, 3, 2, 1)) //false
+    solution(mutableListOf(3, 6, 5, 8, 10, 20, 15)) //false
+    solution(mutableListOf(10, 1, 2, 3, 4, 5)) // true
+    solution(mutableListOf(1, 2, 5, 3, 5)) // true
+    solution(mutableListOf(1, 2, 3, 4, 99, 5, 6)) //true
+    solution(mutableListOf(1, 3,2)) //true
+    solution(mutableListOf(1, 2, 3, 4, 5, 3, 5, 6)) //false
+
 }
 
 private fun solution(sequence: MutableList<Int>): Boolean {
-
-    var valAnterior: Int = 0
-    var contador: Int = 0
-    var respuesta: Boolean = false
-
-    for ((position, value) in sequence.withIndex()){
-
-        if(valAnterior >= value && position >0){
-            contador += 1
+    var numErr = 0
+    var resultado: Boolean = false
+    for (i in 0 until sequence.size - 1) {
+        if (sequence[i] - sequence[i + 1] >= 0) {
+            numErr += 1
+            if (i - 1 >= 0 && i + 2 <= sequence.size - 1 && sequence[i] - sequence[i + 2] >= 0 && sequence[i - 1] - sequence[i + 1] >= 0) {
+                println(resultado)
+                return false
+            }
         }
-
-        valAnterior = value
-
     }
 
-    if(contador <=1){
-        respuesta = true
+    if(numErr <= 1){
+        resultado = true
     }
-    println(respuesta)
-    return respuesta
-
+    println(resultado)
+    return resultado
 }
